f8d9813db49e48e13a573c9040935781dd4bacff
DWKim67 <dkim67@my.yorku.ca>
2024-01-03 10:49:24 -0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Daniel Kim on 2024-01-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var cardCount: Int = 2
+    let emojis: Array<String> = ["👻","🎃","🕷️","😈","💀","🕸️"]
+    var body: some View {
+        VStack{
+            ScrollView {
+                cards
+            }
+            
+            Spacer()
+            cardCountAdjusters
+        }
+        
+        .padding()
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index], isFaceUp: true)
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+            
+        }.foregroundColor(.orange)
+    }
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+        }.imageScale(.large)
+            .font(.largeTitle)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+                cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.fill.badge.plus")
+    }
+}
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = false
+    
+    var body: some View{
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.foregroundColor(.white)
+                
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            }.opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
