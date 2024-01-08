6ae40e10ef856d6b61bd38baa6d1a41c6f3f5ec8
DWKim67 <dkim67@my.yorku.ca>
2024-01-08 11:19:28 -0500

    Refactored A2

---
+++ Memorize/EmojiTheme.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiTheme.swift
+//  Memorize
+//
+//  Created by Daniel Kim on 2024-01-08.
+//
+
+import Foundation
+
+struct Theme {
+    let name: String
+    var emojis: [String]
+    var numPairs: Int
+    let color: String
+}
