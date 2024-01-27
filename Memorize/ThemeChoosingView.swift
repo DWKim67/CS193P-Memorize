ddddf76f6343425cf0dc16c7cd48d7230e1da5ec
DWKim67 <dkim67@my.yorku.ca>
2024-01-27 14:21:46 -0500

    Started work on separating themes

---
+++ Memorize/ThemeChoosingView.swift
@@ -0,0 +1,18 @@
+//
+//  ThemeChoosingView.swift
+//  Memorize
+//
+//  Created by Daniel Kim on 2024-01-26.
+//
+
+import SwiftUI
+
+struct ThemeChoosingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ThemeChoosingView()
+}
